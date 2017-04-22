711079967602edf1db92917cbc019615e9b64024
Mohannad Adham <mohannad.adm@gmail.com>
2017-04-22 14:38:12 -0700

    Add my biography to the main element of index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+	"name" : "Mohannad",
+	"role" : "Geoinformatics specialist",
+	"email" : "mohannad.adm@gmail.com",
+	"message" : "Welcome to my world!",
+	"skills" : ["python", "JavaScript", "HTML", "CSS", "VBA", "SQL"]
+}
+
+text = bio.name + "<br/>" + bio.role + "<br/>" + bio.email + "<br/>" + bio.message;
+
+$("#main").append(text);
